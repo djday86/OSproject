a6f069c05b3ea9cc75733dccaeea6364dced8743
bigdan4712 <bigdan4712@comcast.net>
2017-03-15 14:04:49 -0400

    Made some major modifications to the main file, the program no longer uses "read_bytes" function.  Added a struct that contains the whole VDI header. variables are available inside of the struct".

--- main.c
+++ main.c
@@ -7,36 +7,54 @@
 #include <fcntl.h>
 
 
-#define DRIVE_SIZE_LOC 0x170  //drive size location address
-#define PAGE_SIZE_LOC 0x178 //page size location address
-#define MBR_SIZE 448 //Size of MBR
-
-u64 get_drive_size(u32 fd);
-u32 get_page_size(u32 fd);
-
 typedef struct { 
 	
-	u64 drive_size;
-	u32 page_size;
+	u8 header[0x4b];
+	u32 drive_type;
+	u8 image_desc[0x103];
+	u32 offset_blocks;
+	u32 offset_data;
+	u32 cylinders;
+	u32 heads;
+	u32 sectors;
+	u32 sector_size;
+	u32 unused;
+	u64 disk_size;
+	u32 block_size;
+	u32 block_edata;
+	u32 total_blocks;
+	u32 blocks_allocated;
+	u8 unused_info[0x78];
 
-} disk_information;
+} VDI_header;
 	
-
-
-
 
 int main(int argc, char *argv[]) {
 
 	u32 fd =0;
-	disk_information disk_info;
+	VDI_header disk_info;
 
 	printf("Name of file: %s\n", argv[1]);
 
 	fd = open(argv[1], O_RDONLY);
 
-	disk_info.drive_size = get_drive_size(fd);
-	disk_info.page_size = get_page_size(fd);
+	if(lseek(fd,0,SEEK_SET) == -1) return EXIT_FAILURE;
+	if(read(fd,&disk_info,sizeof(disk_info)) == -1) return EXIT_FAILURE;
 
+	if(disk_info.drive_type == 1) printf("File type: Dynamic\n");
+	else printf("File type: Static\n");
+
+	printf("Offset blocks: %i bytes.\n", disk_info.offset_blocks);
+	printf("Offset data: %i bytes.\n", disk_info.offset_data);
+	printf("Cylinders: %i.\n", disk_info.cylinders);
+	printf("Heads: %i.\n", disk_info.heads);
+	printf("Sectors: %i.\n", disk_info.sectors);
+	printf("Sector Size: %i.\n", disk_info.sector_size);
+	printf("Total disk size: %llu. bytes\n", disk_info.disk_size);	
+	printf("Disk block size: %i bytes.\n", disk_info.block_size);
+	printf("Total # of blocks: %i.\n",disk_info.total_blocks);
+	printf("Total # of blocks allocated: %i.\n",disk_info.blocks_allocated);
+	
 	if(close(fd) == -1) {
 		printf("Error.\n");
 		return EXIT_FAILURE;
@@ -44,41 +62,6 @@ int main(int argc, char *argv[]) {
 
 	return EXIT_SUCCESS;
 
-}
-
-u64 get_drive_size(u32 fd) {
-
-	u8 HDDsize[8] = {0};
-	u64 size = 0;
-
-	if(lseek(fd,DRIVE_SIZE_LOC,SEEK_SET) == -1) return -1;
-	if(read(fd,HDDsize,8) == -1) return -1;
-	
-	for(u32 i=sizeof(HDDsize);i>0;i--) {
-		size = size * 256 + HDDsize[i-1];
-	}
-
-	printf("The size of the drive is: %llu bytes or %llu MiB.\n", size,size/1048576);
-	return size;
-	
-}
-
-u32 get_page_size(u32 fd) {
-
-	u8 pageSize[4] = {0};
-	u32 size = 0;
-
-	if(lseek(fd,PAGE_SIZE_LOC,SEEK_SET) == -1) return -1;
-	if(read(fd,pageSize,4) == -1) return -1;
-	
-	for(u32 i=sizeof(pageSize);i>0;i--) {
-		size = size * 256 + pageSize[i-1];
-	}
-
-	printf("The size of the pages are: %u bytes.\n", size);
-
-	return size;
-	
 }
 
 	
