c0b2d1f39953bae115ffbafdb644c47e3e422b1c
mafreeman02 <esplin11@gmail.com>
2017-03-20 23:56:28 -0400

    Added Superblock Struct and Read

--- main.c
+++ main.c
@@ -8,6 +8,45 @@
 
 u32 i;
 
+struct ext2_super_block {
+    u32   s_inodes_count;         /* Inodes count */
+    u32   s_blocks_count;         /* Blocks count */
+    u32   s_r_blocks_count;       /* Reserved blocks count */
+    u32   s_free_blocks_count;    /* Free blocks count */
+    u32   s_free_inodes_count;    /* Free inodes count */
+    u32   s_first_data_block;     /* First Data Block */
+    u32   s_log_block_size;       /* Block size */
+    s32   s_log_frag_size;        /* Fragment size */
+    u32   s_blocks_per_group;     /* # Blocks per group */
+    u32   s_frags_per_group;      /* # Fragments per group */
+    u32   s_inodes_per_group;     /* # Inodes per group */
+    u32   s_mtime;                /* Mount time */
+    u32   s_wtime;                /* Write time */
+    u16   s_mnt_count;            /* Mount count */
+    s16   s_max_mnt_count;        /* Maximal mount count */
+    u16   s_magic;                /* Magic signature */
+    u16   s_state;                /* File system state */
+    u16   s_errors;               /* Behaviour when detecting errors */
+    u16   s_minor_rev_level;      /* minor revision level */
+    u32   s_lastcheck;            /* time of last check */
+    u32   s_checkinterval;        /* max. time between checks */
+    u32   s_creator_os;           /* OS */
+    u32   s_rev_level;            /* Revision level */
+    u16   s_def_resuid;           /* Default uid for reserved blocks */
+    u16   s_def_resgid;           /* Default gid for reserved blocks */
+}super_block;
+
+typedef struct {
+    u32 bg_block_bitmap;
+    u32 bg_inode_bitmap;
+    u32 bg_inode_table;
+    u16 bg_free_blocks;
+    u16 bg_free_inodes;
+    u16 bg_used_dirs_count;
+    u16 bg_pad;
+    u8 bg_reserved[4];
+    
+} bg_descriptor_table;
 typedef struct { 
 	
 	u8 header[0x4b];
@@ -120,7 +159,7 @@ void get_partition_details(int *part_num partition_entry *data){
          part_num = 1;
          return;
         }
-    else if (diskinfo.partition_table[2].tyep == 0x38){
+    else if (diskinfo.partition_table[2].type == 0x38){
         part_num = 2;
         return;
     }
@@ -131,5 +170,16 @@ void get_partition_details(int *part_num partition_entry *data){
                 
 }
 
+void get_super_block(super_block *main_super_block){
+    lseek(fd, disk_info offsetdata + 1024, SEEK_SET);
+    read(fd, &main_super_block, 1024)
+}
 
-	
+void get_bg_descriptor_table(bg_descriptor_table *bg_data) {
+    
+    lseek(fd, disk_info.offsetdata +)
+}
+
+void fetch_block( void *buf, int block_num){
+    
+}
