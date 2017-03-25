c9ed9ec5bc975a1515764e964ed410a4eba30efa
bigdan4712 <bigdan4712@comcast.net>
2017-03-24 20:47:59 -0400

    Added a function that reads into a buffer generically, we can now use this function to read in any other structs that we may need for the program.  Also changed the super_block file to the actual ext2fs_super_block header and it now can read in all 1024 of the super block.

--- datatypes.h
+++ datatypes.h
@@ -7,11 +7,14 @@
 typedef char s8;
 typedef unsigned char u8;
 
+
 typedef short int s16;
 typedef unsigned short int u16;
 
+
 typedef int s32;
 typedef unsigned int u32;
+
 
 typedef long long int s64;
 typedef unsigned long long int u64;
--- main.c
+++ main.c
@@ -6,37 +6,80 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
 u32 i;
 
 typedef struct {
-
-    u32   s_inodes_count;         /* Inodes count */
-    u32   s_blocks_count;         /* Blocks count */
-    u32   s_r_blocks_count;       /* Reserved blocks count */
-    u32   s_free_blocks_count;    /* Free blocks count */
-    u32   s_free_inodes_count;    /* Free inodes count */
-    u32   s_first_data_block;     /* First Data Block */
-    u32   s_log_block_size;       /* Block size */
-    s32   s_log_frag_size;        /* Fragment size */
-    u32   s_blocks_per_group;     /* # Blocks per group */
-    u32   s_frags_per_group;      /* # Fragments per group */
-    u32   s_inodes_per_group;     /* # Inodes per group */
-    u32   s_mtime;                /* Mount time */
-    u32   s_wtime;                /* Write time */
-    u16   s_mnt_count;            /* Mount count */
-    s16   s_max_mnt_count;        /* Maximal mount count */
-    u16   s_magic;                /* Magic signature */
-    u16   s_state;                /* File system state */
-    u16   s_errors;               /* Behaviour when detecting errors */
-    u16   s_minor_rev_level;      /* minor revision level */
-    u32   s_lastcheck;            /* time of last check */
-    u32   s_checkinterval;        /* max. time between checks */
-    u32   s_creator_os;           /* OS */
-    u32   s_rev_level;            /* Revision level */
-    u16   s_def_resuid;           /* Default uid for reserved blocks */
-    u16   s_def_resgid;           /* Default gid for reserved blocks */
-
-} super_block;
+	int	s_inodes_count;		/* Inodes count */
+	int	s_blocks_count;		/* Blocks count */
+	int	s_r_blocks_count;	/* Reserved blocks count */
+	int	s_free_blocks_count;	/* Free blocks count */
+	int	s_free_inodes_count;	/* Free inodes count */
+	int	s_first_data_block;	/* First Data Block */
+	int	s_log_block_size;	/* Block size */
+	int	s_log_frag_size;	/* Fragment size */
+	int	s_blocks_per_group;	/* # Blocks per group */
+	int	s_frags_per_group;	/* # Fragments per group */
+	int	s_inodes_per_group;	/* # Inodes per group */
+	int	s_mtime;		/* Mount time */
+	int	s_wtime;		/* Write time */
+	short int	s_mnt_count;		/* Mount count */
+	short int	s_max_mnt_count;	/* Maximal mount count */
+	short int	s_magic;		/* Magic signature */
+	short int	s_state;		/* File system state */
+	short int	s_errors;		/* Behaviour when detecting errors */
+	short int	s_minor_rev_level; 	/* minor revision level */
+	int	s_lastcheck;		/* time of last check */
+	int	s_checkinterval;	/* max. time between checks */
+	int	s_creator_os;		/* OS */
+	int	s_rev_level;		/* Revision level */
+	short int	s_def_resuid;		/* Default uid for reserved blocks */
+	short int	s_def_resgid;		/* Default gid for reserved blocks */
+	/*
+	 * These fields are for EXT2_DYNAMIC_REV superblocks only.
+	 *
+	 * Note: the difference between the compatible feature set and
+	 * the incompatible feature set is that if there is a bit set
+	 * in the incompatible feature set that the kernel doesn't
+	 * know about, it should refuse to mount the filesystem.
+	 * 
+	 * e2fsck's requirements are more strict; if it doesn't know
+	 * about a feature in either the compatible or incompatible
+	 * feature set, it must abort and not try to meddle with
+	 * things it doesn't understand...
+	 */
+	int	s_first_ino; 		/* First non-reserved inode */
+	short int   s_inode_size; 		/* size of inode structure */
+	short int	s_block_group_nr; 	/* block group # of this superblock */
+	int	s_feature_compat; 	/* compatible feature set */
+	int	s_feature_incompat; 	/* incompatible feature set */
+	int	s_feature_ro_compat; 	/* readonly-compatible feature set */
+	unsigned char	s_uuid[16];		/* 128-bit uuid for volume */
+	char	s_volume_name[16]; 	/* volume name */
+	char	s_last_mounted[64]; 	/* directory where last mounted */
+	int	s_algorithm_usage_bitmap; /* For compression */
+	/*
+	 * Performance hints.  Directory preallocation should only
+	 * happen if the EXT2_COMPAT_PREALLOC flag is on.
+	 */
+	unsigned char	s_prealloc_blocks;	/* Nr of blocks to try to preallocate*/
+	unsigned char	s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
+	unsigned short int	s_padding1;
+	/*
+	 * Journaling support valid if EXT3_FEATURE_COMPAT_HAS_JOURNAL set.
+	 */
+	unsigned char	s_journal_uuid[16];	/* uuid of journal superblock */
+	unsigned int	s_journal_inum;		/* inode number of journal file */
+	unsigned int	s_journal_dev;		/* device number of journal file */
+	unsigned int	s_last_orphan;		/* start of list of inodes to delete */
+	unsigned int	s_hash_seed[4];		/* HTREE hash seed */
+	unsigned char	s_def_hash_version;	/* Default hash version to use */
+	unsigned char	s_reserved_char_pad;
+	unsigned short int	s_reserved_word_pad;
+	int	s_default_mount_opts;
+ 	int	s_first_meta_bg; 	/* First metablock block group */
+	unsigned int	s_reserved[190];	/* Padding to the end of the block */
+} ext2_super_block;
 
 typedef struct {
 
@@ -69,10 +112,16 @@ typedef struct {
 	u32 total_blocks;
 	u32 blocks_allocated;
 	u8 unused_info[0x78];
-	u32 cursor;
-	s32 *map;
 
 } VDI_header;
+
+typedef struct {
+
+  VDI_header hdr;
+  u32 cursor,fd;
+  u32 *map;
+
+} VDI_file;
 
 typedef struct {
 
@@ -95,57 +144,68 @@ typedef struct __attribute__((packed)) BootSector {
 } BootSector;
 
 
-u32 read_VDI_map(u32 fd, VDI_header *disk_info);
-u32 get_partition_details(u32 fd, VDI_header disk_info, int *part_num, BootSector *data);
-u32 get_super_block(u32 fd, super_block *main_super_block, VDI_header disk_info);
-u32 VDI_translate(u32 desired_byte, VDI_header disk_info);
+u32 read_VDI_map(u32 fd, VDI_file *disk_info, u32 offset_blocks, u32 blocks_allocated);
+u32 get_partition_details(u32 fd, VDI_file disk_info, BootSector boot_sector);
+u32 get_super_block(u32 fd, ext2_super_block *main_super_block, VDI_header disk_info);
+u32 VDI_translate(u32 desired_byte, VDI_file disk_info);
+u32 read_into_buffer(u32 fd, void *buff, u32 position, u32 num_bytes);
 	
 
 int main(int argc, char *argv[]) {
-
+	
+	u32 start;
 	u32 fd =0;
 	VDI_header disk_info;
-	super_block main_super_block;
+	ext2_super_block main_super_block;
+	VDI_file vdi;
 	BootSector boot_sector;
 
-	disk_info.cursor = 0;
-
 	printf("Name of file: %s\n", argv[1]);
+	vdi.fd = open(argv[1], O_RDONLY);
 
-	//lseek(fd,OFFSET_BLOCKS, SEEK_SET)
-	//lseek(fd,LOCATION, SEEK_SET)
-	//read(fd, buffer, size) make sure buffer reading into has enough bytes to store size.
+	if(read_into_buffer(vdi.fd,&vdi.hdr,0,sizeof(vdi.hdr)) == -1) {
+		printf("Error.\n");
+		return EXIT_FAILURE;
+	}
 
-	fd = open(argv[1], O_RDONLY);
+	if(read_VDI_map(vdi.fd,&vdi,vdi.hdr.offset_blocks, vdi.hdr.blocks_allocated) == -1) { 
+		printf("Error.\n");
+		return EXIT_FAILURE;
+	}
 
-	if(lseek(fd,0,SEEK_SET) == -1) return EXIT_FAILURE;
-	if(read(fd,&disk_info,sizeof(disk_info)) == -1) return EXIT_FAILURE;
-	if(read_VDI_map(fd,&disk_info) == -1) return EXIT_FAILURE;
-	//if(get_super_block(fd, &main_super_block, &disk_info) == -1) return EXIT_FAILURE;
-	if(lseek(fd, VDI_translate(0, disk_info), SEEK_SET) == -1) return EXIT_FAILURE;
-	if(read(fd, &boot_sector, 512) == -1) return EXIT_FAILURE;	
+	if(read_into_buffer(vdi.fd,&boot_sector,VDI_translate(0, vdi),512) == -1) {
+		printf("Error.\n");
+		return EXIT_FAILURE;
+	}
 
-	if(lseek(fd, VDI_translate(1024, disk_info), SEEK_SET) == -1) return EXIT_FAILURE;
-    	if(read(fd, &main_super_block, 84) == -1) return EXIT_FAILURE;
+	start = get_partition_details(vdi.fd,vdi,boot_sector);
+
+	if(start == -1) { 
+		printf("Error.\n");
+		return EXIT_FAILURE;
+	}
 	
-
-	if(disk_info.drive_type == 1) printf("File type: Dynamic\n");
+	if(lseek(vdi.fd, VDI_translate(1024+start, vdi), SEEK_SET) == -1) return EXIT_FAILURE;
+    	if(read(vdi.fd, &main_super_block, 1024) == -1) return EXIT_FAILURE;
+	
+	if(vdi.hdr.drive_type == 1) printf("File type: Dynamic\n");
 	else printf("File type: Static\n");
-	printf("Offset blocks: %i bytes.\n", disk_info.offset_blocks);
-	printf("Offset data: %i bytes.\n", disk_info.offset_data);
-	printf("Cylinders: %i.\n", disk_info.cylinders);
-	printf("Heads: %i.\n", disk_info.heads);
-	printf("Sectors: %i.\n", disk_info.sectors);
-	printf("Sector Size: %i.\n", disk_info.sector_size);
-	printf("Total disk size: %llu. bytes\n", disk_info.disk_size);	
-	printf("Disk block size: %i bytes.\n", disk_info.block_size);
-	printf("Total # of blocks: %i.\n",disk_info.total_blocks);
-	printf("Total # of blocks allocated: %i.\n",disk_info.blocks_allocated);
+	printf("Offset blocks: %i bytes.\n", vdi.hdr.offset_blocks);
+	printf("Offset data: %i bytes.\n", vdi.hdr.offset_data);
+	printf("Cylinders: %i.\n", vdi.hdr.cylinders);
+	printf("Heads: %i.\n", vdi.hdr.heads);
+	printf("Sectors: %i.\n", vdi.hdr.sectors);
+	printf("Sector Size: %i.\n", vdi.hdr.sector_size);
+	printf("Total disk size: %llu bytes.\n", vdi.hdr.disk_size);	
+	printf("Disk block size: %i bytes.\n", vdi.hdr.block_size);
+	printf("Total # of blocks: %i.\n",vdi.hdr.total_blocks);
+	printf("Total # of blocks allocated: %i.\n",vdi.hdr.blocks_allocated);
 
 	printf("INFO FROM SUPERBLOCK\n");
-	printf("%i",boot_sector.magic);
+	printf("%i\n",main_super_block.s_mtime);
+	printf("\n%04x\n",main_super_block.s_magic);
 
-	free(disk_info.map);
+	free(vdi.map);
 
 	if(close(fd) == -1) {
 		printf("Error.\n");
@@ -156,9 +216,9 @@ int main(int argc, char *argv[]) {
 
 }
 
-u32 read_VDI_map(u32 fd, VDI_header *disk_info) {
+u32 read_VDI_map(u32 fd, VDI_file *disk_info, u32 offset_blocks, u32 blocks_allocated) {
 
-	disk_info->map = (u32 *)malloc(4*(disk_info->blocks_allocated));
+	disk_info->map = (u32 *)malloc(4*(blocks_allocated));
 
 	if(disk_info->map == NULL) {
 
@@ -166,65 +226,61 @@ u32 read_VDI_map(u32 fd, VDI_header *disk_info) {
 		return -1;
 	}
 
-	if(lseek(fd,disk_info->offset_blocks,SEEK_SET) == -1) return -1;
-	if(read(fd, disk_info->map, 4* disk_info->blocks_allocated) == -1) return -1;
+	if(lseek(fd,offset_blocks,SEEK_SET) == -1) return -1;
+	if(read(fd, disk_info->map, 4*blocks_allocated) == -1) return -1;
 }
 
-u32 VDI_translate(u32 desired_byte, VDI_header disk_info) {
+u32 VDI_translate(u32 desired_byte, VDI_file disk_info) {
 
 	u32 page;
 	u32 offset;
 	u32 loc;
 	u32 frame;
 
-	page = (desired_byte / disk_info.offset_blocks);
-	offset = (desired_byte % disk_info.offset_blocks);
+	page = (desired_byte / disk_info.hdr.offset_blocks);
+	offset = (desired_byte % disk_info.hdr.offset_blocks);
 	frame = disk_info.map[page];
-	loc = disk_info.offset_data + (frame * disk_info.offset_blocks) + offset;
+	loc = disk_info.hdr.offset_data + (frame * disk_info.hdr.offset_blocks) + offset;
 
 	return loc; 
 	
 }
 
-u32 get_partition_details(u32 fd, VDI_header disk_info, int *part_num, BootSector *data){
+u32 get_partition_details(u32 fd, VDI_file disk_info, BootSector boot_sector){
     
-    
-    if(lseek(fd, disk_info.offset_blocks, SEEK_SET) == -1) return -1;
-    if(read(fd, &data, sizeof(data)) == -1) return -1;
-    if(data->partitionTable[0].type == 0x38) return -1;
-    else if(data->partitionTable[1].type == 0x38){
-         part_num = 1;
-         return -1;
-        }
-    else if (data->partitionTable[2].type == 0x38){
-        part_num = 2;
-        return -1;
-    }
-    else if(data->partitionTable[3].type == 0x38) {
-        part_num = 3;
-        return -1;
-    }
+	for (i=0;i<4;i++) {
+		if(boot_sector.partitionTable[i].type == 0x83)
+			break;
+	}	
+	return boot_sector.partitionTable[i].firstSector * 512;
                 
 }
 
-u32 get_super_block(u32 fd, super_block *main_super_block, VDI_header disk_info) {
+/*u32 get_super_block(u32 fd, super_block *main_super_block, VDI_header disk_info) {
 
     if(lseek(fd, ((disk_info.map[0] + 1) * disk_info.offset_blocks) + 1024, SEEK_SET) == -1) return -1;
     if(read(fd, &main_super_block, 84) == -1) return -1;
 
-}
+}*/
 
-u32 get_bg_descriptor_table(u32 fd, int bg_number, super_block main_super_block, bg_descriptor_table *bg_data, VDI_header disk_info) {
+u32 get_bg_descriptor_table(u32 fd, int bg_number, ext2_super_block main_super_block, bg_descriptor_table *bg_data, VDI_header disk_info) {
     
     if(lseek(fd, disk_info.offset_data + 1024 + bg_number * main_super_block.s_blocks_per_group * main_super_block.s_log_block_size + 			main_super_block.s_log_block_size , SEEK_SET) == -1) return -1;
     if(read(fd, &bg_data, sizeof(bg_descriptor_table)) == -1) return -1;
 
 }
 
-u32 fetch_block( u32 fd, void *buf, int block_group, int block_num, VDI_header disk_info, super_block main_super_block) {
+u32 fetch_block( u32 fd, void *buf, int block_group, int block_num, VDI_header disk_info, ext2_super_block main_super_block) {
 
     if(lseek(fd, disk_info.offset_data + 1024 + main_super_block.s_blocks_per_group * block_group * main_super_block.s_log_block_size + 			main_super_block.s_log_block_size * (block_num - 1),SEEK_SET) == -1) return -1;
 
     if(read(fd, &buf, main_super_block.s_log_block_size) == -1) return -1;
     
+}
+
+u32 read_into_buffer(u32 fd, void *buff, u32 position, u32 num_bytes) {
+	
+	if(lseek(fd, position, SEEK_SET) == -1) return -1;
+	if(read(fd, buff, num_bytes) == -1) return -1;
+
 }
